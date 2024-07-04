d7932b9a5a976eecb77c22798435d2eeb7d06e92
lordyemight <lordyemight@gmail.com>
2024-07-04 15:20:58 +0100

    Initial commit with project files

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+
+const IbmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight:['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "A modern banking platform for all",
+  icons: {
+    icon:'/icons/logo.svg'
+  }
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${IbmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
